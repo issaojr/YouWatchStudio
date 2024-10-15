65b92edfd0bd473c9b366c93e80ec852ed030881
Issao Hanaoka Junior <issaojr@gmail.com>
2024-10-14 21:41:48 -0300

    Add project files.

---
+++ YouWatchStudio/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace YouWatchStudio
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
